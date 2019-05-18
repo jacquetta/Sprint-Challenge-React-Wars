9574ee5358f57a1c2c3ef36796d4e4783b7a9143
jacquetta <jacquettavb@hotmail.com>
2019-05-18 12:33:49 -0500

    created new React Wars project

---
+++ starwars2/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      starwarsChars: []
+    };
+  }
+
+  componentDidMount(){
+    this.getCharacters('https://swapi.co/api/people/');
+  }
+
+  getCharacters = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+
+      .then(data => {
+       this.setState({ starwarsChars: data.results });
+      })
+
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  render(){
+    return (
+      <div className='App'>
+        <h1 className="Header">React Wars</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
